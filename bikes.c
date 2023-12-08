3f50217eb39ecf533b6647dc1ee685c38f8e43c0
procerr <ldicandia@itba.edu.ar>
2023-12-08 02:35:02 -0300

    load puts station and getDay functions
    Co-Authored-By: fferrari02 <153206437+fferrari02@users.noreply.github.com>
    Co-Authored-By: keodubo115 <153146325+keodubo115@users.noreply.github.com>
    Co-Authored-By: matepirola <132600198+matepirola@users.noreply.github.com>

--- bikes.c
+++ bikes.c
@@ -56,6 +56,101 @@ typedef struct bikeCDT{
     size_t qtyStations;          //resv_station matriz de adyacencia,      
 }bikeCDT;
 
+static int _strcasecmp(const char *s1, const char *s2)
+{
+    while(*s1 && (toupper((unsigned char)*s1) == toupper((unsigned char)*s2)))
+        s1++, s2++;
+    return toupper((unsigned char)*s1) - toupper((unsigned char)*s2);
+}
+
+static int getDayOfWeek(int day, int month, int year) {
+    if (month < 3) {
+        month += 12;
+        year -= 1;
+    }
+    int k = year % 100;
+    int j = year / 100;
+    int dayOfWeek = (day + 13 * (month + 1) / 5 + k + k / 4 + j / 4 + 5 * j) % 7;
+    return dayOfWeek;
+}
+
+static size_t getDay(char *date ) {
+    
+    char *token = strtok(date, "-");
+    int year = atoi(token);
+    token = strtok(NULL, "-");
+    int month = atoi(token);
+    token = strtok(NULL, "-");
+    int day = atoi(token);
+
+    int dayOfWeek = getDayOfWeek(day, month, year);
+    int days[WEEKS] = {DOM, LUN, MAR, MIE, JUE, VIE, SAB};
+    
+    return days[dayOfWeek];
+}
+
+
+
+static int compareInts(const void *a, const void *b) {
+    int intA = *(const int *)a;
+    int intB = *(const int *)b;
+    return intA - intB;
+}
+
+
+void putStation(bikeADT bike, size_t startId, size_t isMember, char * returnDate, char * startDate,size_t endId, size_t yearFrom, size_t yearTo){
+    size_t newSize = MAYOR(startId, endId);
+
+    if (bike->resv_station < startId || bike->resv_station < endId){
+        bike->station = realloc(bike->station, newSize * sizeof(TVecStation)); // Agrego memoria si es que el station dado es menor a resv_station
+
+        for (int i = bike->resv_station; i < newSize; i++)
+        {
+            bike->station[i].memberTrips = 0;
+            bike->station[i].casualTrips = 0;
+            bike->station[i].allTrips = 0;
+            bike->station[i].nameStation = NULL;
+            bike->station[i].idStation = 0;
+            bike->station[i].bikes = NULL;
+            bike->station[i].oldest.oldestStationId = 0;
+            bike->station[i].oldest.oldestDateTime = NULL;
+            for(int j = 0; j < WEEKS ; j++){
+                bike->qtyPerDay[j].endedTrips = 0;
+                bike->qtyPerDay[j].startedTrips = 0;
+            }
+        }
+        bike->resv_station = newSize;
+    }
+
+//aumentamos cantidades depende si es miembro o no
+    if(isMember){
+        bike->station[startId-1].memberTrips++;
+    }else{
+        bike->station[startId-1].casualTrips++;
+    }
+    bike->station[startId-1].allTrips++;
+
+    bike->qtyPerDay[getDay(startDate)].startedTrips++;  
+    bike->qtyPerDay[getDay(returnDate)].endedTrips++;
+
+   
+    if(bike->station[startId-1].oldest.oldestDateTime == NULL){
+        bike->station[startId - 1].oldest.oldestDateTime = malloc(strlen(startDate)+1);
+        bike->station[startId - 1].oldest.oldestDateTime = strcpy(bike->station[startId - 1].oldest.oldestDateTime, startDate);
+    }
+    else if (strcmp(startDate, bike->station[startId-1].oldest.oldestDateTime) < 0){
+        bike->station[startId - 1].oldest.oldestDateTime = startDate;
+        bike->station[startId - 1].oldest.oldestStationId = endId;
+    }
+}
+
+/*query 1*/
+
+/*query 2*/
+
+/*query 3*/
+
+/*query 4*/
 
 
 
